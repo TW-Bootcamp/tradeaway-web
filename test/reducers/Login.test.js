99d8c910cbf3d8fe3b0b6f8f05059872f0025115
Vikash <vikashkv@thoughtworks.com>
2017-01-18 16:04:02 +0530

    Vikash/Swati | Test Login reducer

---
+++ test/reducers/Login.test.js
@@ -0,0 +1,45 @@
+import reducer from '../../src/reducers/Login'
+import * as types from '../../src/constants/ActionTypes'
+
+
+describe('Login Reducer', () => {
+    it('should return the initial state', () => {
+        expect(
+            reducer(undefined, {})
+        ).toEqual({})
+    });
+
+    it('should handle LOGIN_SUCCESS', () => {
+        expect(
+            reducer({}, {
+                type: types.LOGIN_SUCCESS,
+                payload: "foobarbaz"
+            })
+        ).toEqual({success: true, authToken: "foobarbaz"});
+    });
+    it('should handle LOGIN_FAILED', () => {
+        expect(
+            reducer({}, {
+                type: types.LOGIN_FAILED,
+                payload: "unauthorized"
+            })
+        ).toEqual({success: false, message: "unauthorized"});
+    });
+    it('should handle USER_SUCCESS', () => {
+        var user = {role: "seller"};
+        expect(
+            reducer({success: true}, {
+                type: types.USER_SUCCESS,
+                payload: user
+            })
+        ).toEqual({success: true, user});
+    });
+    it('should handle USER_FAILED', () => {
+        expect(
+            reducer({}, {
+                type: types.USER_FAILED,
+                payload: "not authorized"
+            })
+        ).toEqual({success: false, message: "not authorized"});
+    });
+});
