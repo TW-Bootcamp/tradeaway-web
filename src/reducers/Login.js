694d7a45278cf45f89baa348ca198f291d6cfcdd
Vikash <vikashkv@thoughtworks.com>
2017-01-18 10:39:15 +0530

    Vikash | Update action type in reducer

--- src/reducers/Login.js
+++ src/reducers/Login.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes';
 const initialState = {};
 export default (state = initialState, action) => {
     switch (action.type) {
-        case types.LOGIN:
+        case types.LOGIN_SUCCESS:
             return {
                 authToken: action.payload,
                 success: true
